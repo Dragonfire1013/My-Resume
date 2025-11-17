c1c2aa6f33851dcee33880cb15177e2be2c60a01
Dragonfire1013 <luca.santayana@gmail.com>
2025-11-17 17:59:05 -0500

    Add files via upload

---
+++ script-work-2.js
@@ -0,0 +1,27 @@
+function education() {
+    const edu = document.getElementById('education');
+    const skills = document.getElementById('skills');
+    const whenUgrowup = document.getElementById('careergoals');
+
+    edu.style.display = "block";
+    skills.style.display = "none";
+    whenUgrowup.style.display = "none";
+};
+function myskills() {
+    const edu0 = document.getElementById('education');
+    const skills0 = document.getElementById('skills');
+    const whenUgrowup0 = document.getElementById('careergoals');
+
+    edu0.style.display = "none";
+    skills0.style.display = "block";
+    whenUgrowup0.style.display = "none";
+};
+function careergoals() {
+    const edu1 = document.getElementById('education');
+    const skills1 = document.getElementById('skills');
+    const whenUgrowup1 = document.getElementById('careergoals');
+
+    edu1.style.display = "none";
+    skills1.style.display = "none";
+    whenUgrowup1.style.display = "block";
+};
